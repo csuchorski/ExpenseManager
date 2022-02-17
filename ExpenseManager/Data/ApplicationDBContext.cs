915c74acf042a8c30150ee1bf5bbdf32e8775bf2
Cezary Suchorski <cezary.suchorski@wp.pl>
2022-02-17 17:53:37 +0100

    Added DB context with connection string, changed Startup config

---
+++ ExpenseManager/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using ExpenseManager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- ExpenseManager/Startup.cs
+++ ExpenseManager/Startup.cs
@@ -1,6 +1,8 @@
+using ExpenseManager.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,7 @@ namespace ExpenseManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDBContext>(options=>options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 

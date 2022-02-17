fce450566945b5f764c4b06dcbec0891cc7f4bc7
Cezary Suchorski <cezary.suchorski@wp.pl>
2022-02-17 17:22:39 +0100

    Added borrowed item model

---
+++ ExpenseManager/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Borrower { get; set; } 
+    }
+}

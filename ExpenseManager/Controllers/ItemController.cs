0b3d858d3a2bd92583770e6cc106d9df714bd272
Cezary Suchorski <cezary.suchorski@wp.pl>
2022-02-20 13:19:28 +0100

    Added CreateItem View and functional Create Actions

--- ExpenseManager/Controllers/ItemController.cs
+++ ExpenseManager/Controllers/ItemController.cs
@@ -16,7 +16,20 @@ namespace ExpenseManager.Controllers
             IEnumerable<Item> objList = _db.Items;
             return View(objList);
         }
-
+        //Get Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Post Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+            _db.Items.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
         public ItemController(ApplicationDBContext db)
         {
             _db = db;
--- ExpenseManager/Models/Item.cs
+++ ExpenseManager/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace ExpenseManager.Models
         public int Id { get; set; }
         public string Borrower { get; set; }
         public string Lender { get; set; }
+        [DisplayName("Item name")]
         public string ItemName { get; set; }
     }
 }
